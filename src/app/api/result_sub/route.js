3bc3d7af8721d49f424f5deaf510db8cff19c77e
Vishnu7602 <vishnuvardhan7602@gmail.com>
2024-02-02 23:06:48 +0530

    final commit

--- src/app/api/result_sub/route.js
+++ src/app/api/result_sub/route.js
@@ -1,24 +1,13 @@
 import getMongoConnection from "@/components/database";
-import { revalidatePath } from 'next/cache';
+import { revalidatePath } from "next/cache";
 
-
-async function getAllStudents(db) {
-  // Assuming you have a collection named 'students' in your MongoDB
-  const studentsCollection = db.collection('students');
-  
-  // Retrieve all documents from the 'students' collection
-  const students = await studentsCollection.find().toArray();
-
-  return students;
-}
 
 export async function GET(request) {
-  console.log(request);
-  try {
+   console.log(request);
+  try { 
     const db = await getMongoConnection();
-    const students = await getAllStudents(db);
-    const path = request.nextUrl.searchParams.get('path')
-    revalidatePath(path);
+    const students = await db.collection('students').find().toArray();
+    revalidatePath('/api/result_sub');
 
     return new Response(JSON.stringify(students), { status: 200 });
   } catch (error) {
